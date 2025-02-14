6f9859b2479e219a98eea14338505f9f7b996115
Michael Wendell <79287734+mswendell@users.noreply.github.com>
2025-02-13 17:43:14 -0700

    Delete ktb-main directory

--- ktb-main/KTB_App/Kids Tech Balance/Kids Tech Balance/Models/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  Kids Tech Balance
-//
-//  Created by Michael Wendell on 4/23/24.
-//
-
-import Foundation
--- ktb-main/KTB_App/Kids Tech Balance/Monitor/MyMonitor.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MyMonitor.swift
-//  Monitor
-//
-//  Created by Michael Wendell on 4/14/24.
-//
-
-import Foundation
